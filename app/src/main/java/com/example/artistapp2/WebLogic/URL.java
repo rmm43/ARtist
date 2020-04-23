0ae78c5c88cfb2b1fc44c00f384b231bd42ad8f4
RyanM <rmm43@students.uwf.edu>
2020-04-22 22:02:24 -0400

    Added webcall for adding users into the database

--- app/src/main/java/com/example/artistapp2/WebLogic/URL.java
+++ app/src/main/java/com/example/artistapp2/WebLogic/URL.java
@@ -4,5 +4,5 @@ public class URL {
 
     static String addFriendURL = "";
     static String getFriendsList = "ec2-3-21-34-236.us-east-2.compute.amazonaws.com:4848/friends/";
-
+    static String createNewUser = "ec2-3-21-34-236.us-east-2.compute.amazonaws.com:4848/users/create";
 }
--- app/src/main/java/com/example/artistapp2/WebLogic/Webcall.java
+++ app/src/main/java/com/example/artistapp2/WebLogic/Webcall.java
@@ -2,6 +2,7 @@ package com.example.artistapp2.WebLogic;
 
 import android.content.Context;
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
 
 import cz.msebera.android.httpclient.Header;
@@ -11,14 +12,15 @@ public class Webcall {
     public static void webCall(final Context context, String requestURL, String userId)
     {
         String finalURL = requestURL;
-        if(requestURL.equals(URL.getFriendsList))
-        {
+        if(requestURL.equals(URL.getFriendsList)) {
             finalURL += userId;
             final AsyncHttpClient client = new AsyncHttpClient();
             client.get(finalURL, null, new TextHttpResponseHandler() {
                 @Override
                 public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                    //
+
+                    //TODO add in a connection error message
+
                 }
 
                 @Override
@@ -28,8 +30,26 @@ public class Webcall {
                 }
             });
         }
+    }
 
+    public static void addUserWebcall(String username, String uid, String email)
+    {
+        RequestParams params = new RequestParams();
+        params.put("user_id", uid);
+        params.put("username", username);
+        params.put("email", email);
 
+        final AsyncHttpClient client = new AsyncHttpClient();
+        client.post(URL.createNewUser, params, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                //TODO add in a connection error message
+            }
 
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                //Do nothing, it succeeded.
+            }
+        });
     }
 }
